9c86efad2732805bf6b11d46bc422c4762ac13b9
Labiba Ghani <bibazzcore@gmail.com>
2026-09-13 21:46:45 +0500

    Add Lab 1

---
+++ Lab 1/1.py
@@ -0,0 +1,25 @@
+print("hello")
+
+# im labiba
+
+a=input("enter a value:")
+print(a)
+print(type(a))
+
+b=int(input("enter a value:"))
+print(b)
+print(type(b))
+
+z=4+6j
+print(type(z))
+
+z="python"
+print(z[0:4])
+print(z[-3:-1])
+
+my_list=['red','green', "blue"]
+print(my_list)
+print(my_list[1])
+print(my_list[-2])
+print(my_list[0:4])
+print(my_list[-3:-1])
